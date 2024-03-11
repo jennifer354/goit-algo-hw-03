6c225c4105897b7a491b3025c87711dc5156e81b
jennifer354 <157841543+jennifer354@users.noreply.github.com>
2024-03-11 19:22:57 +0100

    date fixed

--- ex1.py
+++ ex1.py
@@ -1,13 +1,15 @@
 #Завантажуєм модуль
 from datetime import datetime
 #Створюєм функцію
-def get_days_from_today (now_date, end_date):
-    diff = now_date - end_date
+def get_days_from_today (now_date, date):
+    diff = now_date - date
     return diff.days
 #Вписуєм параметри для дат
-end_date = datetime(year=2023, month=12, day=18)
+my_date ='2000-01-01'
+date = datetime.strptime(my_date, '%Y-%m-%d')
+print(date.month)
 now_date = datetime.now()
 #Задаєм параметри функціі
-days = get_days_from_today (now_date, end_date)
+days = get_days_from_today (now_date, date)
 #Виносимо результат
-print(f'get_days_from_today,{end_date} {days}  days')
+print(f'get_days_from_today,{my_date} {days}  days')
---
+++ ex_2.py
@@ -0,0 +1,7 @@
+
+import random
+
+
+numbers = (1,49,6)
+team = random.sample(numbers, 6)
+print(f"Команда: {team}")
