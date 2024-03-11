3a2b8f0da097220295f9363a05abf34c7c7b2502
jennifer354 <157841543+jennifer354@users.noreply.github.com>
2024-03-11 22:21:22 +0100

    допрацювати

--- ex_2.py
+++ ex_2.py
@@ -1,7 +1,10 @@
-
 import random
+def get_numbers_ticket (l):
+l = list(range(1, 36))
+print(l)
+random.shuffle(l)
+print(l)
+print(random.choice(l))
+print(random.sample(l, k=5))
+print (get_numbers_ticket)
 
-
-numbers = (1,49,6)
-team = random.sample(numbers, 6)
-print(f"Команда: {team}")
